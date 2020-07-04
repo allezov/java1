48928468ea91e704b1d3903ef32b5d355e339f85
OUT-Lezov-AE <Lyezov.A.E@sberbank.ru>
2020-07-04 12:33:52 +0300

    Корректировка итоговой задачи №1

--- src/FromBinaryToDecimal.java
+++ src/FromBinaryToDecimal.java
@@ -5,27 +5,37 @@ public class FromBinaryToDecimal {
     public static void main(String[] args) {
 
         Scanner in = new Scanner(System.in);
-        System.out.println("Укажите длину бинарного числа");
-        int arrSize = in.nextInt();
-        int[] arr = new int[arrSize];
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println("Укажите " + (i + 1) + " бит бинарного числа");
-            arr[i]=in.nextInt();
-         if (arr[i] > 1 | arr[i] < 0){
-             System.out.println("Введите либо 1 либо 0");
-             i--;
-         }
-        }
-        System.out.println("Введеное бинарное число "+Arrays.toString(arr));
+        boolean flag = false;
+        int[] myIntArray;
+        String inputBinary;
+        do {
+            System.out.println("Введите бинарное число");
+            inputBinary = in.nextLine();
+            String[] arrayBinary = inputBinary.split("");
+            myIntArray = Arrays.stream(arrayBinary).mapToInt(Integer::parseInt).toArray();
+            for (int value : myIntArray) {
+                if (value > 1 | value < 0) {
+                    System.out.println("Введенное число не является бинарным");
+                    flag = false;
+                    break;
+                } else {
+                    flag = true;
+                }
+            }
+        } while (!flag);
+
+        System.out.println("Введеное бинарное число " + inputBinary);
+
         int sum = 0;
         int count = 1;
-            for (int i = arr.length - 1; i >= 0; i--) {
-                if (arr[i] == 1) {
+            for (int i = myIntArray.length - 1; i >= 0; i--) {
+                if (myIntArray[i] == 1) {
                     sum += count;
                 }
                 count *= 2;
             }
             System.out.println("Бинарное число в десятичном виде " + sum);
-        }
     }
+    }
+
 
